ae18168b9823f965c11dc75f14e3faa89b0ff966
Stefan <gauchoaleman@yahoo.com>
2019-08-13 20:21:09 -0300

    user system working

---
+++ README.md
@@ -0,0 +1,9 @@
+# miweb
+Libro diario hecho con Laravel
+
+Ejecutar esto para instalar:
+php artisan make:auth
+php artisan migrate
+
+Mirar:
+https://github.com/thedevdojo/chatter
---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+
+@include('includes/navbar')
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header"></div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    Estás logueado!
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
