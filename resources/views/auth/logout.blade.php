83ae17b8e6a8da55498eea19e22174913c380624
Stefan <gauchoaleman@yahoo.com>
2019-08-22 20:11:33 -0300

    correcciones user system

--- resources/views/auth/logout.blade.php
+++ resources/views/auth/logout.blade.php
@@ -5,7 +5,7 @@ Session::flush();
             <meta charset="utf-8">
             <meta name="viewport" content="width=device-width, initial-scale=1">
 
-            <title>Diario</title>
+            <title>Autoayuda</title>
 
             <!-- Fonts -->
             <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -5,17 +5,17 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+                <div class="card-header">{{ __('Verifique su Email') }}</div>
 
                 <div class="card-body">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
+                            {{ __('Un link de verificación ha sido enviado a su email.') }}
                         </div>
                     @endif
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }}, <a href="{{ route('verification.resend') }}">{{ __('click here to request another') }}</a>.
+                    {{ __('Antes de procedes, revise su email para el link de verificación.') }}
+                    {{ __('Si no ha recibido el email') }}, <a href="{{ route('verification.resend') }}">{{ __('clik aquí para pedir otro') }}</a>.
                 </div>
             </div>
         </div>
