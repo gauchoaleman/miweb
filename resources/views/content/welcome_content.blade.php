350b8b8e1899fa5af5a6024a9281d92b1ff7d499
Stefan <gauchoaleman@yahoo.com>
2019-09-26 19:15:18 -0300

    titulo

--- resources/views/content/welcome_content.blade.php
+++ resources/views/content/welcome_content.blade.php
@@ -15,7 +15,7 @@
 
     <div class="content">
       <div class="title m-b-md" style="color:orange">
-          Autoayuda Suicidios<br>
+          Desde tu lugar se puede prevenir<br>
           <img src="/img/autoayuda_portada.jpg">
 
       </div>
