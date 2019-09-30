830ed017bc5c72e4542e0679e34133cccaddfaef
Stefan <gauchoaleman@yahoo.com>
2019-09-30 20:41:36 -0300

    correccion bottom

--- resources/views/includes/bottom.blade.php
+++ resources/views/includes/bottom.blade.php
@@ -2,3 +2,5 @@
 <script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/4.17.47/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment-with-locales.js"></script>
 <script src="//cdn.rawgit.com/Eonasdan/bootstrap-datetimepicker/e8bddc60e73c1ec2475f827be36e1957af72e2ea/src/js/bootstrap-datetimepicker.js"></script>
+</body>
+</html>
