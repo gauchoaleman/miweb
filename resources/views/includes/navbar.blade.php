78763116c6c49a3cdd6b517dcc9c1c27a720d287
Stefan <gauchoaleman@yahoo.com>
2019-10-01 21:19:19 -0300

    dropdown categorias

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -4,44 +4,6 @@
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 
-
-<!--
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-
-  <a class="navbar-brand" href="http://localhost:8000"><img src='http://localhost:8000/img/home.png'><b>Home</b></a>||&nbsp;
-
-
-    @if (Route::has('login'))
-    @auth
-  <div class="navbar-brand" ><img src='http://localhost:8000/img/hello.png'>Hola {{{ isset(Auth::user()->name) ? Auth::user()->name : Auth::user()->email }}} ||&nbsp; </div>
-  <div class="navbar-brand" ><a class="navbar-brand" href="http://localhost:8000/calendar/user/view_events_user"><img src='http://localhost:8000/img/calendar.png'>Eventos</a> ||&nbsp; </div>
-
-    @if ( (Auth::user()->is_admin == 1) )
-<a class="navbar-brand" href="http://localhost:8000/auth/admin/view">
-      <img src='http://localhost:8000/img/user_crud.png'>ABM Usuarios&nbsp;|
-</a>
-<a class="navbar-brand" href="http://localhost:8000/calendar/admin/view_events">
-      <img src='http://localhost:8000/img/calendar_crud.png'>ABM Eventos&nbsp;|
-</a>
-    @endif
-
-  @endauth
-  @endif
-
-    @if (Route::has('login'))
-      <div>
-    @auth
-    <a class="navbar-brand" href="http://localhost:8000/logout"><img src='http://localhost:8000/img/logout.png'>Logout</a>|&nbsp;
-    <a class="navbar-brand" href="http://localhost:8000/auth/user/update_data"><img src='http://localhost:8000/img/update_data.jpeg'>Actualizar datos</a>|&nbsp;
-    @else
-        <a class="navbar-brand" href="{{ route('login') }}"><img src='http://localhost:8000/img/login.png'>Login</a>&nbsp;<a class="navbar-brand" href="{{ route('register') }}">|<img src='http://localhost:8000/img/register.png'> Registrarse</a>
-    @endauth
-    </div>
-    @endif
-</nav>
--->
-
-
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="http://localhost:8000" style="color:orange"><img src='http://localhost:8000/img/home.png'><b>Home</b></a>&nbsp;
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -56,6 +18,20 @@
       <li class="nav-item active">
         <div class="navbar-brand" ><a class="navbar-brand" style="color:orange" href="http://localhost:8000/calendar/user/view_events_user"><img src='http://localhost:8000/img/calendar.png'>Eventos</a> &nbsp; </div>
       </li>
+      <?php
+      $categories = DB::table('categories')->get();
+       ?>
+      <div class="dropdown">
+          <a class="navbar-brand" valign="bottom" style="color:orange" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Documentos
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <?php
+            foreach ($categories as $category) {
+             ?><a class="dropdown-item" style="color:orange" href="http://localhost:8000/documents?category_id=<?php echo $category->id ?>"><?php echo $category->name ?></a>
+           <?php } ?>
+          </div>
+      </div>
 
       @if ( (Auth::user()->is_admin == 1) )
       <li class="nav-item active">
