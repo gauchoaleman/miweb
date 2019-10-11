7aafbd670dd767e5072c560b5c140b025593f805
Stefan <gauchoaleman@yahoo.com>
2019-10-11 13:07:15 -0300

    letra quienes somos

--- resources/views/content/welcome_content.blade.php
+++ resources/views/content/welcome_content.blade.php
@@ -3,8 +3,10 @@
           Desde tu lugar se puede prevenir<br>
         </div>
 <hr>
-          <div class="title m-b-md" style="color:orange" align="center" valign="top"><b>Quienes somos</b></div>
-          La siguiente página surge de un equipo de profesionales con la idea de enfocarnos en la Ley Nacional de Prevención del Suicidio 27.130, específicamente en el eje de la prevención.<br>
+          <div class="title m-b-md" style="color:orange" align="center" valign="top"><h3>Quienes somos</h3></div>
+La siguiente página surge de un equipo de profesionales con la idea de enfocarnos en la Ley Nacional de Prevención del Suicidio 27.130, específicamente en el eje de la prevención.<br>
 Brindaremos asistencia gratuita, personal, confidencial, y anónima a personas en crisis o riesgo de suicidio, familiares.<br>
-Ademas otras actividades brindaremos cursos, talleres y asesoramiento a docentes e instituciones en temas relacionados con la ideación suicida adolescente y autolesiones No suicidas. Se concientizara y sensibilizara a la población de la problemática de suicidio en general con fines de prevención. Para finalizar nos regimos con el paradigma biopsicosocial y espiritual por lo tanto somos respetuosos de todos los credos e ideologías.
+Ademas otras actividades brindaremos cursos, talleres y asesoramiento a docentes e instituciones en temas relacionados con la ideación suicida adolescente y autolesiones No suicidas.<br>
+Se concientizara y sensibilizara a la población de la problemática de suicidio en general con fines de prevención.<br>
+Para finalizar nos regimos con el paradigma biopsicosocial y espiritual por lo tanto somos respetuosos de todos los credos e ideologías.
 </div>
