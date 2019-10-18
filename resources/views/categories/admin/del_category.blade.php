673237a0157a4051464768fbbba18a2f6dae7189
Stefan <gauchoaleman@yahoo.com>
2019-10-18 14:47:45 -0300

    borrar categoria

--- resources/views/categories/admin/del_category.blade.php
+++ resources/views/categories/admin/del_category.blade.php
@@ -1,5 +1,12 @@
 @include('includes/head')
-<?php DB::table('categories')->where('id',$_GET['id'])->delete() ?>
+<?php
+DB::table('categories')->where('id','=',$_GET['id'])->delete();
+$documents = DB::table('documents')->where('category_id',$_GET['id'])->get();
+foreach($documents as $document){
+  unlink($_SERVER['DOCUMENT_ROOT']."/documents/".$document->title.".".$document->extension);
+}
+DB::table('documents')->where('category_id','=',$_GET['id'])->delete();
+?>
 @include('includes/navbar')
 <div class='high_text' style='color:orange'>Categoría borrada</div>
 @include('categories.admin.tables.view_categories_admin_table')
