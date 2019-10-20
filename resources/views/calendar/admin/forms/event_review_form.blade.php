86ca11979a32e0d7f1e575b997b3fc6aa0a3639d
Stefan <gauchoaleman@yahoo.com>
2019-10-20 18:34:36 -0300

    correcciones css navbar

--- resources/views/calendar/admin/forms/event_review_form.blade.php
+++ resources/views/calendar/admin/forms/event_review_form.blade.php
@@ -27,25 +27,28 @@ $event = DB::table('events')->where('id', $_GET['id'])->first();
                         <div class="form-group row">
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Foto 1') }}</label>
 
+
+                            @if( ! get_event_pic_url($_GET['id'],1))
                             <div class="col-md-6">
                                 {{ Form::file('pic1') }}
-
                             </div>
-                            <?php if( $foto1_url = get_event_pic_url($_GET['id'],1)){?>
+                            @endif
+                            @if( $foto1_url = get_event_pic_url($_GET['id'],1))
                                     <img src="{{$foto1_url}}" width=75 align=right>
                                     <a href="/calendar/admin/del_event_picture?id={{$_GET['id']}}&pic_id=1">
                                       <img src="http://{{$_SERVER['HTTP_HOST']}}/img/delete.png" width=10 height=10 align=right>
                                     </a>
-                          <?php } ?>
-
+                            @endif
                         </div>
+
                         <div class="form-group row">
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Foto 2') }}</label>
 
+                            @if( ! get_event_pic_url($_GET['id'],2))
                             <div class="col-md-6">
                                 {{ Form::file('pic2') }}
-
                             </div>
+                            @endif
                           @if( $foto2_url = get_event_pic_url($_GET['id'],2))
                                     <img src="{{$foto2_url}}" width=75 align=right>
                                     <a href="/calendar/admin/del_event_picture?id={{$_GET['id']}}&pic_id=2">
--- resources/views/includes/bottom_bar.blade.php
+++ resources/views/includes/bottom_bar.blade.php
@@ -1,5 +1,9 @@
 <br>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" style="color:orange" href="http://{{$_SERVER['HTTP_HOST']}}/contact/contact"><img src='http://{{$_SERVER['HTTP_HOST']}}/img/contact.png'>Contacto</a>&nbsp;
-  <a class="navbar-brand" style="color:orange" href="http://{{$_SERVER['HTTP_HOST']}}"><img src='http://{{$_SERVER['HTTP_HOST']}}/img/download_app.png'>Bajar App</a>&nbsp;
+  <div onmouseover="this.style.background='#ffdca4';" onmouseout="this.style.background='#F8F8F8';" class="navbar_item">
+    <a class="navbar-brand" style="color:orange" href="http://{{$_SERVER['HTTP_HOST']}}/contact/contact"><img src='http://{{$_SERVER['HTTP_HOST']}}/img/contact.png'>Contacto</a>&nbsp;
+  </div>
+  <div onmouseover="this.style.background='#ffdca4';" onmouseout="this.style.background='#F8F8F8';" class="navbar_item">
+    <a class="navbar-brand" style="color:orange" href="http://{{$_SERVER['HTTP_HOST']}}"><img src='http://{{$_SERVER['HTTP_HOST']}}/img/download_app.png'>Bajar App</a>&nbsp;
+  </div>
 </nav>
